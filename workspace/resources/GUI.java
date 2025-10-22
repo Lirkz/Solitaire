08abf3a9fbca25dc135c461af9e30dc345233804
Lirkz <96188080+Lirkz@users.noreply.github.com>
2025-10-22 16:42:09 +0000

    Steps 1 and 2 done

--- workspace/resources/GUI.java
+++ workspace/resources/GUI.java
@@ -1,3 +1,4 @@
+//David Saiontz GUI for the blackjack game, steps 1 and 2 are just drawing the boxes and a card stack
 package resources;
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -11,6 +12,7 @@ import java.awt.event.MouseMotionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Stack;
 
 
 public class GUI extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
@@ -22,6 +24,8 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
        setTitle("Solitaire");
        setSize(900,700);
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   
+
        
        // this supplies the background
        try {
@@ -39,11 +43,79 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
         * game once it's fully created.
         */
        Card card = new Card(2, Card.Suit.Diamonds);
+	   Card card2  = new Card(3, Card.Suit.Spades);
+	   Card card3 = new Card(4, Card.Suit.Hearts);
+	   Card card4 = new Card(5, Card.Suit.Clubs);
+	   Stack<Card> cards = new Stack<>();
+	   cards.push(card);
+	   cards.push(card2);
+	   cards.push(card3);
+	   cards.push(card4);
        System.out.println(card);
-       this.add(card);    
-
-        this.setVisible(true);
+       ///this.add(card);    
+	   GridBagConstraints oppC =  new GridBagConstraints();
+	   GridBagConstraints deckC = new GridBagConstraints();
+	   GridBagConstraints playC = new GridBagConstraints();
+	   GridBagConstraints cardsC = new GridBagConstraints();
+	   JPanel outer = new JPanel();
+	   this.add(outer);
+	   outer.setOpaque(false);
+	   outer.setLayout(new GridBagLayout());
+	   outer.setSize(new Dimension(750,500));
+	   outer.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.RED));
+	   JPanel opponent = new JPanel();
+	   opponent.setLayout(new GridBagLayout());
+	   oppC.gridheight =2;
+	   oppC.gridwidth = 3;
+	   oppC.anchor = GridBagConstraints.PAGE_START;
+	   oppC.weighty = 1;
+	   oppC.ipady = 175;
+	   oppC.ipadx = 150;
+	   opponent.setOpaque(false);
+	   opponent.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.PINK));
+	   JPanel deck = new JPanel();
+	   deckC.gridx = 2;
+	   deckC.gridy = 2;
+	   deckC.weighty = 2;
+	   deckC.ipadx = 75;
+	   deckC.ipady = 100;
+	   deck.setOpaque(false);
+	   deck.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.YELLOW));
+	   JPanel player= new JPanel();
+	   playC.gridheight =2;
+	   playC.gridwidth = 3;
+	   playC.gridy =3;
+	   playC.ipadx = 150;
+	   playC.ipady = 175;
+	   player.setOpaque(false);
+	   player.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.BLUE));
+	   outer.add(opponent,oppC);
+	   outer.add(deck, deckC);
+	   outer.add(player,playC);
+	   outer.add(drawPile(cards));
+	   cardsC.anchor = GridBagConstraints.FIRST_LINE_START;
+	   cardsC.weighty=1;
+	   cardsC.weightx =1;
+		cardsC.gridheight = 1;
+       cardsC.gridwidth = 1;
+	   cardsC.ipadx = 84;
+	   cardsC.ipady = 120;
+	   opponent.add(drawPile(cards),cardsC);
+    	this.setVisible(true);
     }
+	public JLayeredPane drawPile(Stack<Card> stackIn) {
+    	Object cards[];
+    	cards = stackIn.toArray(); //please note we convert this stack to an array so that we can iterate through it backwards while drawing. You’ll need to cast each element inside cards to a <Card> in order to use the methods to adjust their position
+		JLayeredPane pane = new JLayeredPane();
+		for (int i = 0;i<cards.length;i++){
+
+			((Card)cards[i]).setSize(new Dimension(49, 70));
+			((Card)cards[i]).setBounds(i*8, i*14, 49, 70);
+			pane.add((Card)cards[i],0);
+		}
+		
+		return pane;
+	}
 
 
 	@Override
